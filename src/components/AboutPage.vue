a34767ada666bd360a045209f2d18513b0f89a58
tameemahamed <ahamedtameem00@gmail.com>
2025-10-11 02:14:10 +0600

    added markdown parsing in about pages

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -4,9 +4,11 @@
             <h2 class="h2 article-title">About me</h2>
         </header>
         <section class="about-text">
-            <p v-for="(p, index) in data.about_paragraphs" :key="index">
-                {{ p.text }}
-            </p>
+            <p 
+                v-for="(p, index) in data.about_paragraphs" 
+                :key="index" 
+                v-html="renderMarkdown(p.text)" 
+            />                
         </section>
         <section class="service">
             <h3 class="h3 service-title">What I'm doing</h3>
@@ -26,7 +28,13 @@
 </template>
 
 <script setup>
+import { marked } from 'marked';
+
 defineProps({
     data: Object
 });
+
+const renderMarkdown = (text) => {
+    return marked.parse(text || "")
+};
 </script>
